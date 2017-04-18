3c564ea81e40bf9ec28468f3af64c978b53802dc
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-18 04:57:05 +0300

    0.0.1

---
+++ Signer.php
@@ -0,0 +1,26 @@
+<?php
+namespace Dfe\Paypal;
+/**
+ * 2017-04-18
+ * @see \Dfe\Paypal\Signer\Request
+ * @see \Dfe\Paypal\Signer\Response
+ * @method Settings s()
+ */
+abstract class Signer extends \Df\PaypalClone\Signer {
+	/**
+	 * 2017-04-18
+	 * @used-by sign()
+	 * @see \Dfe\Paypal\Signer\Request::values()
+	 * @see \Dfe\Paypal\Signer\Response::values()
+	 * @return string[]
+	 */
+	abstract protected function values();
+
+	/**
+	 * 2017-04-18
+	 * @override
+	 * @see \Df\PaypalClone\Signer::sign()
+	 * @return string
+	 */
+	final protected function sign() {return implode($this->values());}
+}
---
+++ Signer/Request.php
@@ -0,0 +1,13 @@
+<?php
+namespace Dfe\Paypal\Signer;
+// 2017-04-18
+final class Request extends \Dfe\Paypal\Signer {
+	/**
+	 * 2017-04-18
+	 * @override
+	 * @see \Dfe\Paypal\Signer::values()
+	 * @used-by \Dfe\Paypal\Signer::sign()
+	 * @return string[]
+	 */
+	protected function values() {return dfa_select_ordered($this->v(), []);}
+}
